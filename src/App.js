e69a862b6b91f7fefefa8a2a89ed5271372b2031
isaacwnsolomon <g00424127@atu.ie>
2024-10-04 09:29:10 +0000

    Excerise 6

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         <Route path="/read" element={<Footer/>} />
         <Route path="/create" element={<Header/>} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
