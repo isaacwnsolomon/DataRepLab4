566408c07fd86fd4e769a48762b2c9b0edf26b9b
isaacwnsolomon <g00424127@atu.ie>
2024-10-04 09:24:13 +0000

    Exercise 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/navbar';
 import Header from './components/header';
-import Content from './components/content';
 import Footer from './components/footer';
+import Content from './components/content';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Content />
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<Footer/>} />
+        <Route path="/create" element={<Header/>} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
