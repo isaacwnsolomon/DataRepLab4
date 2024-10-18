fb32b05091be463c5f78d14b6ce8fc043e10765c
isaacwnsolomon <g00424127@atu.ie>
2024-10-18 09:29:39 +0000

    Exercise 3 done

--- src/components/create.js
+++ src/components/create.js
@@ -4,11 +4,16 @@ import { useState } from "react";
 
 function Create() {
   const [title, setTitle] = useState('');
+  const[year, setYear] = useState('');
+  const[poster,setPoster] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(title);
+    console.log(year);
+    console.log(poster);
   }
+  
 
   return (
     <div>
@@ -22,11 +27,42 @@ function Create() {
             value={title}
             onChange={(e) => { setTitle(e.target.value) }}
           />
+        
         </div>
         <input type="submit" value="Add Movie" />
+      </form>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Year: </label>
+          <input type="text"
+            className="form-control"
+            // Value set to title 
+            value={year}
+            onChange={(e) => { setYear(e.target.value) }}
+          />
+        
+        </div>
+        <input type="submit" value="Add Year" />
+      </form>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Poster URL: </label>
+          <input type="text"
+            className="form-control"
+            // Value set to title 
+            value={poster}
+            onChange={(e) => { setPoster(e.target.value) }}
+          />
+        
+        </div>
+        <input type="submit" value="Add Poster" />
       </form>
     </div>
   );
 }
 
 export default Create;
+
+//comments on usestate, use
