dd4c9af0ff656e00fbc50b36075c32eda85dc578
isaacwnsolomon <g00424127@atu.ie>
2024-10-18 09:13:00 +0000

    Exercise 3a and 3b done

--- src/components/create.js
+++ src/components/create.js
@@ -1,7 +1,32 @@
-// Footer arrow function 
-const Create = () => {
-    // Returns text
-      return <h3>Hello from the create component!</h3>;
-    };
-  // Exporting so we can use elsewhere
-    export default Create;
+// create.js
+
+import { useState } from "react";
+
+function Create() {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            // Value set to title 
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
+}
+
+export default Create;
